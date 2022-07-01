ffe462ef5780c5db6ebe1954612f5b054f3f5401
Anton Romashkin <tony201994@mail.ru>
2022-07-01 13:25:04 +0400

    Fix class name

--- test/controllers/web/sessions_controller_test.rb
+++ test/controllers/web/sessions_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Web::SessionsControllerTest < ActionDispatch::IntegrationTest
+class Web::SessionsControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
