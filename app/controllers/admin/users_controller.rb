452f152ec5b45ed78f67eaf06c062bb7e6bc716e
Anton Romashkin <tony201994@mail.ru>
2022-07-02 15:52:14 +0400

    Add list users page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,10 @@
 class Admin::UsersController < Admin::ApplicationController
-  def show
+    def index
+        @q = User.ransack(params[:q])
+        @users = @q.result.order(:type).order(:id).page(params[:page]).per(10)
+    end
+
+    def show
     @user = User.find(params[:id])
   end
 end
