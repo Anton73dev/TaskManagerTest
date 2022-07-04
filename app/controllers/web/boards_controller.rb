afa4646cf40b85df4e7c1b20ddd10b867be35e2b
Anton Romashkin <tony201994@mail.ru>
2022-07-04 11:36:59 +0400

    Add authentication for the board

--- app/controllers/web/boards_controller.rb
+++ app/controllers/web/boards_controller.rb
@@ -1,3 +1,10 @@
 class Web::BoardsController < Web::ApplicationController
+  before_action :authenticate_user!
+
+  setup do
+    user = create(:user)
+    sign_in user
+  end
+
   def show; end
 end
