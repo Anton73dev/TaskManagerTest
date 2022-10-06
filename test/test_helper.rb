94be64b0cee48e901eafb64e47c7939f761e7b14
Anton Romashkin <tony201994@mail.ru>
2022-10-07 00:10:50 +0400

    Add Coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,10 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'coveralls'
+Coveralls.wear!('rails')
+
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
