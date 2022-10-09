3681f82d56aa25a97f3961eb82df13d8c159d6e5
Anton Romashkin <tony201994@mail.ru>
2022-10-09 18:19:58 +0400

    Fix

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,8 @@ require 'simplecov'
 require 'simplecov-lcov'
 # require 'coveralls'
 
-p 111111111111111111111111111111111111
-p Rails.root.join('coverage/lcov.info')
+# p 111111111111111111111111111111111111
+# p Rails.root.join('coverage/lcov.info')
 
 SimpleCov.start do
   SimpleCov::Formatter::LcovFormatter.config do |c|
