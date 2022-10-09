054ede1c29f6d2ad4f42eb755b63dd917b6376c1
Anton Romashkin <tony201994@mail.ru>
2022-10-09 18:27:29 +0400

    Fix

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,7 @@ require 'simplecov-lcov'
 SimpleCov.start do
   SimpleCov::Formatter::LcovFormatter.config do |c|
     c.report_with_single_file = true
-    c.single_report_path = Rails.root.join('coverage/lcov.info')
+    c.single_report_path = 'coverage/lcov.info'
   end
 
   formatter SimpleCov::Formatter::LcovFormatter
